776d3c6be5cde21d042b240bb557483a68aa9583
Bogdan Pop <bcpop_work@gmail.com>
2026-06-05 14:22:20 +0100

    Refactor page objects to use base page helper methods

--- src/main/java/com/bogdan/automation/pages/HomePage.java
+++ src/main/java/com/bogdan/automation/pages/HomePage.java
@@ -4,96 +4,95 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 /**
- * Represents the Demo Web Shop home page.
+ * Represents the Demo Web Shop homepage.
  */
 public class HomePage extends BasePage {
 
-	private By registerLink = By.cssSelector("a.ico-register");
-	private By loginLink = By.cssSelector("a.ico-login");
-	private By shoppingCartLink = By.cssSelector("a.ico-cart");
-	private By wishlistLink = By.cssSelector("a.ico-wishlist");
-	private By searchBox = By.id("small-searchterms");
+    private By registerLink = By.cssSelector("a.ico-register");
+    private By loginLink = By.cssSelector("a.ico-login");
+    private By logoutLink = By.cssSelector("a.ico-logout");
+    private By accountLink = By.cssSelector("a.account");
+    private By shoppingCartLink = By.cssSelector("a.ico-cart");
+    private By wishlistLink = By.cssSelector("a.ico-wishlist");
+    private By searchBox = By.id("small-searchterms");
 
-	private By accountLink = By.cssSelector("a.account");
-	private By logoutLink = By.cssSelector("a.ico-logout");
+    private By booksCategory = By.cssSelector(".top-menu a[href='/books']");
+    private By computersCategory = By.cssSelector(".top-menu a[href='/computers']");
+    private By electronicsCategory = By.cssSelector(".top-menu a[href='/electronics']");
+    private By apparelShoesCategory = By.cssSelector(".top-menu a[href='/apparel-shoes']");
+    private By digitalDownloadsCategory = By.cssSelector(".top-menu a[href='/digital-downloads']");
+    private By jewelryCategory = By.cssSelector(".top-menu a[href='/jewelry']");
+    private By giftCardsCategory = By.cssSelector(".top-menu a[href='/gift-cards']");
 
-	private By booksCategory = By.cssSelector(".top-menu a[href='/books']");
-	private By computersCategory = By.cssSelector(".top-menu a[href='/computers']");
-	private By electronicsCategory = By.cssSelector(".top-menu a[href='/electronics']");
-	private By apparelShoesCategory = By.cssSelector(".top-menu a[href='/apparel-shoes']");
-	private By digitalDownloadsCategory = By.cssSelector(".top-menu a[href='/digital-downloads']");
-	private By jewelryCategory = By.cssSelector(".top-menu a[href='/jewelry']");
-	private By giftCardsCategory = By.cssSelector(".top-menu a[href='/gift-cards']");
+    public HomePage(WebDriver driver) {
+        super(driver);
+    }
 
-	public HomePage(WebDriver driver) {
-		super(driver);
-	}
+    public boolean isRegisterLinkDisplayed() {
+        return isDisplayed(registerLink);
+    }
 
-	public boolean isRegisterLinkDisplayed() {
-		return driver.findElement(registerLink).isDisplayed();
-	}
+    public boolean isLoginLinkDisplayed() {
+        return isDisplayed(loginLink);
+    }
 
-	public boolean isLoginLinkDisplayed() {
-		return driver.findElement(loginLink).isDisplayed();
-	}
+    public boolean isLogoutLinkDisplayed() {
+        return isDisplayed(logoutLink);
+    }
 
-	public boolean isShoppingCartLinkDisplayed() {
-		return driver.findElement(shoppingCartLink).isDisplayed();
-	}
+    public boolean isAccountLinkDisplayed() {
+        return isDisplayed(accountLink);
+    }
 
-	public boolean isWishlistLinkDisplayed() {
-		return driver.findElement(wishlistLink).isDisplayed();
-	}
+    public boolean isShoppingCartLinkDisplayed() {
+        return isDisplayed(shoppingCartLink);
+    }
 
-	public boolean isSearchBoxDisplayed() {
-		return driver.findElement(searchBox).isDisplayed();
-	}
+    public boolean isWishlistLinkDisplayed() {
+        return isDisplayed(wishlistLink);
+    }
 
-	public boolean isBooksCategoryDisplayed() {
-		return driver.findElement(booksCategory).isDisplayed();
-	}
+    public boolean isSearchBoxDisplayed() {
+        return isDisplayed(searchBox);
+    }
 
-	public boolean isComputersCategoryDisplayed() {
-		return driver.findElement(computersCategory).isDisplayed();
-	}
+    public boolean isBooksCategoryDisplayed() {
+        return isDisplayed(booksCategory);
+    }
 
-	public boolean isElectronicsCategoryDisplayed() {
-		return driver.findElement(electronicsCategory).isDisplayed();
-	}
+    public boolean isComputersCategoryDisplayed() {
+        return isDisplayed(computersCategory);
+    }
 
-	public boolean isApparelShoesCategoryDisplayed() {
-		return driver.findElement(apparelShoesCategory).isDisplayed();
-	}
+    public boolean isElectronicsCategoryDisplayed() {
+        return isDisplayed(electronicsCategory);
+    }
 
-	public boolean isDigitalDownloadsCategoryDisplayed() {
-		return driver.findElement(digitalDownloadsCategory).isDisplayed();
-	}
+    public boolean isApparelShoesCategoryDisplayed() {
+        return isDisplayed(apparelShoesCategory);
+    }
 
-	public boolean isJewelryCategoryDisplayed() {
-		return driver.findElement(jewelryCategory).isDisplayed();
-	}
+    public boolean isDigitalDownloadsCategoryDisplayed() {
+        return isDisplayed(digitalDownloadsCategory);
+    }
 
-	public boolean isGiftCardsCategoryDisplayed() {
-		return driver.findElement(giftCardsCategory).isDisplayed();
-	}
+    public boolean isJewelryCategoryDisplayed() {
+        return isDisplayed(jewelryCategory);
+    }
 
-	public void clickLoginLink() {
-		driver.findElement(loginLink).click();
-	}
+    public boolean isGiftCardsCategoryDisplayed() {
+        return isDisplayed(giftCardsCategory);
+    }
 
-	public boolean isAccountLinkDisplayed() {
-		return isDisplayed(accountLink);
-	}
+    public String getLoggedInAccountEmail() {
+        return getText(accountLink);
+    }
 
-	public String getLoggedInAccountEmail() {
-		return getText(accountLink);
-	}
+    public void clickLoginLink() {
+        click(loginLink);
+    }
 
-	public boolean isLogoutLinkDisplayed() {
-		return isDisplayed(logoutLink);
-	}
-
-	public void clickLogout() {
-		click(logoutLink);
-	}
+    public void clickLogout() {
+        click(logoutLink);
+    }
 }
